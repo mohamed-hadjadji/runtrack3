011384b81edaf5f47cb8468f6ad473536c71c571
mohamed-hadjadji <55872437+mohamed-hadjadji@users.noreply.github.com>
2020-03-10 14:03:46 +0100

    job02 OK

---
+++ jour02/job02/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    
+   <script type="text/javascript" src="script.js"></script>
+
+</head>
+
+<body>
+	<article id="texte" style="visibility: hidden;">L'important n'est pas la chute, mais l'atterrissage</article>
+	<button id="button" onclick="showhide();">click</button>
+	
+</body>
+</html>
+</DOCTYPE>
---
+++ jour02/job02/script.js
@@ -0,0 +1,9 @@
+function showhide()
+{
+
+if (document.getElementById("texte").style.visibility== "visible")
+document.getElementById("texte").style.visibility= "hidden";
+else
+document.getElementById("texte").style.visibility= "visible";
+ 
+}
