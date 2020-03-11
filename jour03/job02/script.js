3a236af5010be9234e93298d82b88fba240d245c
mohamed-hadjadji <55872437+mohamed-hadjadji@users.noreply.github.com>
2020-03-11 20:22:37 +0100

    introduire message "gangné ou perdu"

--- jour03/job02/script.js
+++ jour03/job02/script.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+var bloc=[];
+var bloc2=["arc1","arc2","arc3","arc4","arc5","arc6"];
   
   $("#button").click(function(){
      var parent = $("#melangees");
@@ -7,22 +9,47 @@ $(document).ready(function(){
         parent.append(divs.splice(Math.floor(Math.random() * divs.length), 1)[0]);
     }
   });
-  $("#arc1").click(function(){
-    $("#arc1").appendTo("#rangees");
-   });
-  $("#arc2").click(function(){
-    $("#arc2").appendTo("#rangees");
-   });
-  $("#arc3").click(function(){
-    $("#arc3").appendTo("#rangees");
-   });
-  $("#arc4").click(function(){
-    $("#arc4").appendTo("#rangees");
-   });
-  $("#arc5").click(function(){
-    $("#arc5").appendTo("#rangees");
-   });
-  $("#arc6").click(function(){
-    $("#arc6").appendTo("#rangees");
-   });
-});        
+  $("img").click(function(){
+    $(this).appendTo("#rangees");
+   var count = $("#rangees img").length;
+     
+    bloc.push($(this).attr('id'));
+    
+var i = 1;
+ if (count == 6)
+ {
+    var win= true;
+
+    while( i < 6 ){
+
+         if( bloc[i] != bloc2[i]){
+            win = false;          
+        }
+  i++;    
+  }
+if (win)
+    {
+        var p = document.createElement('p');
+        var newelement = document.createTextNode("Vous avez gagné");
+        p.setAttribute("id", "win");
+
+        document.body.appendChild(p);
+        p.appendChild(newelement);
+    }
+
+    else {
+
+        var p = document.createElement('p');
+        var newelement = document.createTextNode("Vous avez perdu");
+        p.setAttribute("id", "loose");
+        document.body.appendChild(p);
+        p.appendChild(newelement);
+    }
+
+}
+
+
+});
+       
+});
+
