38b6d96dccf4f172434b80f833a48e9bee0a676a
mohamed-hadjadji <55872437+mohamed-hadjadji@users.noreply.github.com>
2020-03-11 14:03:09 +0100

    job02 OK

---
+++ jour03/job02/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    
+<link rel="stylesheet" type="text/css" href="style.css">   
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script type="text/javascript" src="script.js"></script>
+
+</head>
+
+<body>
+<div id="melangees">
+<img id="arc1" src="img/arc1.png">
+<img id="arc2" src="img/arc2.png">
+<img id="arc3" src="img/arc3.png">
+<img id="arc4" src="img/arc4.png">
+<img id="arc5" src="img/arc5.png">
+<img id="arc6" src="img/arc6.png">
+</div>
+<button id="button">mélanger</button>
+<div id="rangees">
+	
+</div>
+
+
+</body>
+</html>
---
+++ jour03/job02/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+  
+  $("#button").click(function(){
+    $("#arc1").insertAfter($("#arc6"));
+    $("#arc2").insertAfter($("#arc4"));
+    $("#arc3").insertBefore($("#arc1"));
+    $("#arc5").insertAfter($("#arc6"));
+  });
+  $("#arc1").click(function(){
+    $("#arc1").appendTo("#rangees");
+   });
+  $("#arc2").click(function(){
+    $("#arc2").appendTo("#rangees");
+   });
+  $("#arc3").click(function(){
+    $("#arc3").appendTo("#rangees");
+   });
+  $("#arc4").click(function(){
+    $("#arc4").appendTo("#rangees");
+   });
+  $("#arc5").click(function(){
+    $("#arc5").appendTo("#rangees");
+   });
+  $("#arc6").click(function(){
+    $("#arc6").appendTo("#rangees");
+   });
+});        
